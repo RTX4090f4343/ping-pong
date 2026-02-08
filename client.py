4dea4725e22e990d65ccecf063507edc684bccb9
RTX4090f4343 <dm04a113@sc4.dp.ua>
2026-02-09 03:12:38 +1400

    Update client.py

--- client.py
+++ client.py
@@ -3,9 +3,16 @@ import socket
 import json
 from threading import Thread
 
+from launcher import launcher
+
+player_name, ball_color = launcher()
+print("Ім'я гравця:", player_name)
+print("Колір шарика:", ball_color)
+
 # ---ПУГАМЕ НАЛАШТУВАННЯ ---
 WIDTH, HEIGHT = 800, 600
 init()
+mixer.init()
 screen = display.set_mode((WIDTH, HEIGHT))
 clock = time.Clock()
 display.set_caption("Пінг-Понг")
@@ -43,12 +50,19 @@ font_main = font.Font(None, 36)
 # --- ЗОБРАЖЕННЯ ----
 
 # --- ЗВУКИ ---
+mixer.music.load("gumboot_blues.flac")
+mixer.music.set_volume(0.4)
+wall_hit_sound = mixer.Sound("knifesharpener2.flac")
+platform_hit_sound = mixer.Sound("knifesharpener2.flac")
+wall_hit_sound.set_volume(0.7)
+platform_hit_sound.set_volume(0.7)
 
 # --- ГРА ---
 game_over = False
 winner = None
 you_winner = None
 my_id, game_state, buffer, client = connect_to_server()
+mixer.music.play(-1)
 Thread(target=receive, daemon=True).start()
 while True:
     for e in event.get():
@@ -60,12 +74,13 @@ while True:
         countdown_text = font.Font(None, 72).render(str(game_state["countdown"]), True, (255, 255, 255))
         screen.blit(countdown_text, (WIDTH // 2 - 20, HEIGHT // 2 - 30))
         display.update()
-        continue  # Не малюємо гру до завершення відліку
+        continue
 
     if "winner" in game_state and game_state["winner"] is not None:
-        screen.fill((20, 20, 20))
+        screen.fill((20, 60, 20))
+        mixer.music.stop()
 
-        if you_winner is None:  # Встановлюємо тільки один раз
+        if you_winner is None:
             if game_state["winner"] == my_id:
                 you_winner = True
             else:
@@ -74,34 +89,34 @@ while True:
         if you_winner:
             text = "Ти переміг!"
         else:
-            text = "Пощастить наступним разом!"
+            text = "Пощастить наступного разу!"
 
         win_text = font_win.render(text, True, (255, 215, 0))
         text_rect = win_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
         screen.blit(win_text, text_rect)
 
-        text = font_win.render('К - рестарт', True, (255, 215, 0))
+        text = font_win.render('К - ерезапуск', True, (255, 215, 0))
         text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 120))
         screen.blit(text, text_rect)
 
         display.update()
-        continue  # Блокує гру після перемоги
+        continue
 
     if game_state:
-        screen.fill((30, 30, 30))
+        screen.fill((20, 60, 20))
+
         draw.rect(screen, (0, 255, 0), (20, game_state['paddles']['0'], 20, 100))
         draw.rect(screen, (255, 0, 255), (WIDTH - 40, game_state['paddles']['1'], 20, 100))
-        draw.circle(screen, (255, 255, 255), (game_state['ball']['x'], game_state['ball']['y']), 10)
+        draw.circle(screen, ball_color,
+                    (game_state['ball']['x'], game_state['ball']['y']), 10)
         score_text = font_main.render(f"{game_state['scores'][0]} : {game_state['scores'][1]}", True, (255, 255, 255))
         screen.blit(score_text, (WIDTH // 2 -25, 20))
 
         if game_state['sound_event']:
             if game_state['sound_event'] == 'wall_hit':
-                # звук відбиття м'ячика від стін
-                pass
+                wall_hit_sound.play()
             if game_state['sound_event'] == 'platform_hit':
-                # звук відбиття м'ячика від платформи
-                pass
+                platform_hit_sound.play()
 
     else:
         wating_text = font_main.render(f"Очікування гравців...", True, (255, 255, 255))
